9f573dd243a84f90aca5bf20a7497fcf190292e6
lghiggino <lghiggino@gmail.com>
2021-08-11 20:09:16 -0300

    removed Date edition

--- src/components/Note.js
+++ src/components/Note.js
@@ -7,7 +7,7 @@ const Note = ({ note, toggleImportance, updateDate, removeNote }) => {
             <span>{note.date.split("T")[0]}</span>
             <span>{note.content}</span>
             <button onClick={toggleImportance}>{label}</button>
-            <button onClick={updateDate}>Update date</button>
+            {/* <button onClick={updateDate}>Update date</button> */}
             <button onClick={removeNote}>Remove note</button>
         </li>
     )
